1a0e4ae630dd25a9ff064b547097f173f463fd56
yon <Lighthawky@gmail.com>
2012-10-17 11:51:19 +0200

    added fonts + title and navbar

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,6 @@ module MitnadvimNet
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
