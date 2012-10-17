9e943b0d8bba30ea526307cdac4d4538ab5bdc27
Ido Feins <ifeins@gmail.com>
2012-10-17 15:46:13 +0200

    birth year temporarily as a text field.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :age, :area_id, :email, :facebook_id, :first_name, :gender, :last_name, :phone_number, :birth_date, :description
+  attr_accessible :age, :area_id, :email, :facebook_id, :first_name, :gender, :last_name, :phone_number, :birth_year, :description
   attr_accessible :area, :school, :school_id, :type
 
   belongs_to :area
