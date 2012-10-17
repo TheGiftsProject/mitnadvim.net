b7a01168c44480a1caec226fc709778a694c7e43
yon <Lighthawky@gmail.com>
2012-10-17 12:53:04 +0200

    working on box mixin

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ MitnadvimNet::Application.routes.draw do
 
   resources :users
 
+  root :to => 'application#temp_root'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,7 +56,7 @@ MitnadvimNet::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
