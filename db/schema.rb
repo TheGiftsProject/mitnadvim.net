510954d810e3615c487c348111c172a993f427b6
Asaf Gartner <agartner@ebay.com>
2012-10-17 12:30:42 +0200

    Scaffolds for request creation page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121015082434) do
+ActiveRecord::Schema.define(:version => 20121017101029) do
 
   create_table "areas", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(:version => 20121015082434) do
     t.integer  "category_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "type"
+    t.string   "recurrence"
   end
 
   create_table "responses", :force => true do |t|
