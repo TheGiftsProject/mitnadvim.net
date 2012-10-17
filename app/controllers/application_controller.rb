ab61702947b950b0c7047b7cd02242e85ad84fca
Ariel Sakin <asakin@ebay.com>
2012-10-17 17:00:01 +0200

    merges

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,14 @@ require 'controller_support/user_support'
 class ApplicationController < ActionController::Base
 
   protect_from_forgery
-  def widgets
+
+  def homepage
+    @open_requests = Request.where(:status => 'open')
+  end
 
   include UserSupport
 
-  def temp_root
+  def widgets
     #flash.now[:notice] = "HEY"
     #flash.now[:error] = "HEY"
   end
