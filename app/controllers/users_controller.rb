251b1c8bb54f5c98cd524921923cf6eeae4d88d9
Shay Davidson <shay.h.davidson@gmail.com>
2012-10-17 15:19:58 +0200

    Changed form to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(params[:user])
-
+    
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
