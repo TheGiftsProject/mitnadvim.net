4bdf21d5973a341a975cfde9ef577774a5f54ca3
Asaf Gartner <agartner@ebay.com>
2012-10-17 16:19:00 +0200

    Added duration to requests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121017133418) do
+ActiveRecord::Schema.define(:version => 20121017141134) do
 
   create_table "areas", :force => true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20121017133418) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
     t.integer  "recurrence_id"
+    t.integer  "duration"
   end
 
   create_table "responses", :force => true do |t|
