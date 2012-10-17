19ea6afcd17805ab3f101bc9e90c48f6acff667e
Adam Farhi <afarhi@ebay.com>
2012-10-17 12:38:11 +0200

    added bootsrap form classes to signup form

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -27,7 +27,7 @@ class SchoolsController < ApplicationController
     @school = School.new
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html # new.html.haml
       format.json { render json: @school }
     end
   end
