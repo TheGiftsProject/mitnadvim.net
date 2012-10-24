b875c7ef2417f4e8cdf809c103df238358c099f3
Ido Feins <ifeins@gmail.com>
2012-10-24 18:33:59 +0200

    Added gender drop down.

--- app/helpers/volunteers_helper.rb
+++ app/helpers/volunteers_helper.rb
@@ -1,12 +1,21 @@
 module VolunteersHelper
 
-  def birth_years(selected_year)
+  def year_options(selected_year)
     years = *(1940..2000)
     options_for_select(years, selected_year)
   end
 
-  def areas(selected)
+  def area_options(selected)
     options_from_collection_for_select(Area.all, :id, :name, selected)
+  end
+
+  def gender_options(selected)
+    genders = {
+      :male => t("users.new.genders.male"),
+      :female => t("users.new.genders.female")
+    }
+
+    options_from_collection_for_select(genders, :first, :second, selected)
   end
 
 end
