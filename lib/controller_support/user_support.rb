a56f4391fa9e92b5647ffc3a584e356d63311182
Ido Feins <ifeins@gmail.com>
2012-11-17 22:54:41 +0200

    Added anonymous flow.

--- lib/controller_support/user_support.rb
+++ lib/controller_support/user_support.rb
@@ -10,8 +10,7 @@ module UserSupport
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] end
 
   def sign_in(user)
     session[:user_id] = user.id
