c5f7cc7940729b37a648ff6344a7502c27d338b5
Dima <dima.michaelov@lehavi.com>
2013-07-02 18:52:47 +0300

    moved all email text into translation files

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,16 +3,16 @@ class RegistrationMailer < ActionMailer::Base
 
   def welcome_user(user_id)
     @user = User.find(user_id)
-    mail(to: @user.email, subject: I18n.t('email.subject.welcome_user')).deliver
+    mail(to: @user.email, subject: I18n.t('email.welcome_user.subject')).deliver
   end
 
   def welcome_school(school_id)
     @school = School.find(school_id)
-    mail(to: @school.admin.email, subject: I18n.t('email.subject.welcome_school')).deliver
+    mail(to: @school.admin.email, subject: I18n.t('email.welcome_school.subject')).deliver
   end
 
   def new_response(response_id)
     @response = Response.find(response_id)
-    mail(to: @response.request.school.admin.email, subject: I18n.t('email.subject.new_response')).deliver
+    mail(to: @response.request.school.admin.email, subject: I18n.t('email.new_response.subject')).deliver
   end
 end
