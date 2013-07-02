8bf6e8e7e7904ec75f9882a9a1ac84a968c394b6
Dima <dima.michaelov@lehavi.com>
2013-07-02 18:39:13 +0300

    change mailer to use with delayed_jobs

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,18 +1,18 @@
 class RegistrationMailer < ActionMailer::Base
   default from: "noreply@mitnadvim.net"
 
-  def welcome_user(user)
-    @user = user
-    mail(to: @user.email, subject: I18n.t('email.subject.welcome_user'))
+  def welcome_user(user_id)
+    @user = User.find(user_id)
+    mail(to: @user.email, subject: I18n.t('email.subject.welcome_user')).deliver
   end
 
-  def welcome_school(school)
-    @school = school
-    mail(to: @school.admin.email, subject: I18n.t('email.subject.welcome_school'))
+  def welcome_school(school_id)
+    @school = School.find(school_id)
+    mail(to: @school.admin.email, subject: I18n.t('email.subject.welcome_school')).deliver
   end
 
-  def new_response(response)
-    @response = response
-    mail(to: @response.request.school.admin.email, subject: I18n.t('email.subject.new_response'))
+  def new_response(response_id)
+    @response = Response.find(response_id)
+    mail(to: @response.request.school.admin.email, subject: I18n.t('email.subject.new_response')).deliver
   end
 end
