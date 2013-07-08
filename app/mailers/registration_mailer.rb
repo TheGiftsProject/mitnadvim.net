83a0d2f37abd8cd61adffb72520dad11194e4e92
Dima <dima.michaelov@lehavi.com>
2013-07-08 14:08:47 +0300

    removed the deliver as delayed_job does that automatically

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -3,16 +3,16 @@ class RegistrationMailer < ActionMailer::Base
 
   def welcome_user(user_id)
     @user = User.find(user_id)
-    mail(to: @user.email, subject: I18n.t('email.welcome_user.subject')).deliver
+    mail(to: @user.email, subject: I18n.t('email.welcome_user.subject'))
   end
 
   def welcome_school(school_id)
     @school = School.find(school_id)
-    mail(to: @school.admin.email, subject: I18n.t('email.welcome_school.subject')).deliver
+    mail(to: @school.admin.email, subject: I18n.t('email.welcome_school.subject'))
   end
 
   def new_response(response_id)
     @response = Response.find(response_id)
-    mail(to: @response.request.school.admin.email, subject: I18n.t('email.new_response.subject')).deliver
+    mail(to: @response.request.school.admin.email, subject: I18n.t('email.new_response.subject'))
   end
 end
