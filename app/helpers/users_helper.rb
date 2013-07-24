eb7f5bc6fc569d1fae68e01b5bb6d047a7070671
Dima <dima.michaelov@lehavi.com>
2013-07-24 16:36:35 +0300

    school options for school drop down

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -18,4 +18,8 @@ module UsersHelper
     options_from_collection_for_select(Area.all, :id, :name, selected)
   end
 
+  def school_options(selected)
+    options_from_collection_for_select(School.all, :id, :name, selected)
+  end
+
 end
