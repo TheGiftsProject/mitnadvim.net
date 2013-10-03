9c9213ce11a7604c12ce3efe49b9dad578bce8d0
Ido Feins <ifeins@gmail.com>
2013-10-03 22:16:50 +0300

    styled the volunteer responded email.

--- app/mailers/activity_mailer.rb
+++ app/mailers/activity_mailer.rb
@@ -2,6 +2,7 @@ class ActivityMailer < BaseMailer
 
   def volunteer_responded(response)
     @response = response
+    @link = school_url(@response.request.school, :host => 'local.mitnadvim.net')
     mail(to: @response.request.school.admin.email, subject: I18n.t('email.volunteer_responded.subject'))
   end
 
