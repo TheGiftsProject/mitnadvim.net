1110c4d304495eba55492aa847392d94313e6e32
Ido Feins <ifeins@gmail.com>
2013-10-15 01:11:13 +0300

    mail host should be taken from environment variable and not be hard-coded.

--- app/mailers/activity_mailer.rb
+++ app/mailers/activity_mailer.rb
@@ -2,7 +2,7 @@ class ActivityMailer < BaseMailer
 
   def volunteer_responded(response)
     @response = response
-    @link = school_url(@response.request.school, :host => 'local.mitnadvim.net')
+    @link = school_url(@response.request.school, :host => ENV['APP_HOST'])
     mail(to: @response.request.school.admin.email, subject: I18n.t('email.volunteer_responded.subject'))
   end
 
--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -2,13 +2,13 @@ class RegistrationMailer < BaseMailer
 
   def welcome_user(user)
     @user = user
-    @link = root_url(:host => 'local.mitnadvim.net')
+    @link = root_url(:host => ENV['APP_HOST'])
     mail(to: @user.email, subject: I18n.t('email.welcome_user.subject'))
   end
 
   def welcome_school(school)
     @school = school
-    @link = school_url(school, :host => 'local.mitnadvim.net')
+    @link = school_url(school, :host => ENV['APP_HOST'])
     mail(to: @school.admin.email, subject: I18n.t('email.welcome_school.subject'))
   end
 
