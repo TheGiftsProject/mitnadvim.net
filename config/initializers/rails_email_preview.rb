720327e9c57951b0a1c2326569f83a929a661013
Ido Feins <ifeins@gmail.com>
2013-10-15 01:39:31 +0300

    this time done right.

--- config/initializers/rails_email_preview.rb
+++ config/initializers/rails_email_preview.rb
@@ -1,10 +1,13 @@
-return unless Rails.env.development?
+configure_email_preview if Rails.env.development?
 
-require 'rails_email_preview'
+def configure_email_preview
+  require 'rails_email_preview'
 
-Rails.application.config.to_prepare do
-  # Auto-load preview classes from:
-  RailsEmailPreview.preview_classes = Dir[Rails.root.join 'app/mailer_previews/*_preview.rb'].map { |p|
-    File.basename(p, '.rb').camelize
-  }
+  Rails.application.config.to_prepare do
+    # Auto-load preview classes from:
+    RailsEmailPreview.preview_classes = Dir[Rails.root.join 'app/mailer_previews/*_preview.rb'].map { |p|
+      File.basename(p, '.rb').camelize
+    }
+  end
 end
+
