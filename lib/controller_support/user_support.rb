804e8f0b5e0607d1108a4d3aa87b1a75357a25de
Ido Feins <ifeins@gmail.com>
2013-10-15 20:41:23 +0300

    fixed user support to not raise error if it can't find user with id.

--- lib/controller_support/user_support.rb
+++ lib/controller_support/user_support.rb
@@ -10,7 +10,8 @@ module UserSupport
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id] end
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id].present?
+  end
 
   def sign_in(user)
     session[:user_id] = user.id
