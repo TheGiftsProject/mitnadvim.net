fe6ef20eec0cf739d3aa23b0aa5cb2e62a0f0c6a
Ido Feins <ifeins@gmail.com>
2013-10-17 10:28:43 +0300

    fixed an issue that the school admin user was not deleted.

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,8 +3,8 @@ class School < ActiveRecord::Base
   attr_accessible :area
 
   belongs_to :area
-  has_one :admin, :class_name => "User"
-  has_many :requests
+  has_one :admin, :class_name => 'User', :dependent => :destroy
+  has_many :requests, :dependent => :destroy
 
   validates :name, length: { minimum: 2 }
 
