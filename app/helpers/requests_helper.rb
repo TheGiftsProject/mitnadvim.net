a3fc1e9d18d88e57a9e67b11c531f894dab99e1d
Dima <dima.michaelov@lehavi.com>
2013-10-30 12:08:55 +0200

    weekday options for weekly modal's day of week selector

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -12,4 +12,15 @@ module RequestsHelper
     options_from_collection_for_select(School.from_area(area), :id, :name, selected)
   end
 
+  def weekday_options
+    {
+        0 => I18n.t('modals.weekly.sunday'),
+        1 => I18n.t('modals.weekly.monday'),
+        2 => I18n.t('modals.weekly.tuesday'),
+        3 => I18n.t('modals.weekly.wednesday'),
+        4 => I18n.t('modals.weekly.thursday'),
+        5 => I18n.t('modals.weekly.friday')
+    }
+  end
+
 end
